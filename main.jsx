ff768460a4bf0dcec51699b3bc4595944d591a3b
Bat-mann <ramniranjan2211@gmail.com>
2026-05-02 12:31:03 +0530

    changes are made

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import SummarizerApp from './summarizer.jsx';
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SummarizerApp />
-  </React.StrictMode>
-);
